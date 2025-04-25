8278c9f313e5c6f869d31028adf3187c46f1accb
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-04-26 01:38:46 +0530

    Initial commit - added Spring Boot and React projects

---
+++ authfy/src/test/java/com/master/authfy/AuthfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.authfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
