b13b5c5c756f91c3105fe29681de7b009d4ff0e9
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-05-14 22:40:40 +0530

    feat(auth): generate JWT token on login and return it as HttpOnly cookie

--- authfy/src/main/java/com/master/authfy/controller/AuthController.java
+++ authfy/src/main/java/com/master/authfy/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.master.authfy.controller;
 
 import com.master.authfy.io.AuthRequest;
+import com.master.authfy.io.AuthResponse;
 import com.master.authfy.service.AppUserDetailsService;
+import com.master.authfy.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +28,7 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final AppUserDetailsService appUserDetailsService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<?> login( @RequestBody AuthRequest request ) {
@@ -30,6 +36,15 @@ public class AuthController {
         try {
             authenticate(request.getEmail (), request.getPassword ());
             final UserDetails userDetails = appUserDetailsService.loadUserByUsername(request.getEmail());
+            final String jwtToken = jwtUtil.generateToken(userDetails);
+            ResponseCookie cookie = ResponseCookie.from ( "jwt" , jwtToken )
+                    .httpOnly ( true )
+                    .path ( "/" )
+                    .maxAge ( Duration.ofDays ( 1 ) )
+                    .sameSite ( "Strict" )
+                    .build ();
+            return ResponseEntity.ok (  ).header ( HttpHeaders.SET_COOKIE , cookie.toString () )
+                    .body ( new AuthResponse ( request.getEmail (), jwtToken ) );
         }catch (BadCredentialsException ex){
 
             Map<String, Object> error = new HashMap<> ();
