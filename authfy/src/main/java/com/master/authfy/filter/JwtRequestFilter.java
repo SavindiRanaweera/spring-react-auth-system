ac044751de0f41c60e86578b133db54389bacd0c
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-07-09 00:23:24 +0530

    validate the token and security context

--- authfy/src/main/java/com/master/authfy/filter/JwtRequestFilter.java
+++ authfy/src/main/java/com/master/authfy/filter/JwtRequestFilter.java
@@ -8,6 +8,10 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -49,6 +53,18 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                         jwt = cookie.getValue();
                         break;
                     }
+                }
+            }
+        }
+
+        if ( jwt != null ) {
+            email = jwtUtil.extractEmail( jwt );
+            if ( email != null && SecurityContextHolder.getContext().getAuthentication() == null ) {
+                UserDetails userDetails = appUserDetailsService.loadUserByUsername( email );
+                if ( jwtUtil.validateToken( jwt, userDetails ) ) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken( userDetails, null, userDetails.getAuthorities() );
+                    authenticationToken.setDetails ( new WebAuthenticationDetailsSource ().buildDetails( request ) );
+                    SecurityContextHolder.getContext().setAuthentication( authenticationToken );
                 }
             }
         }
